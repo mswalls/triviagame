6167fef0274f76ed2ccbb38e5c80823011be8eb6
mswalls <mswalls@rocketmail.com>
2018-07-31 21:23:37 -0400

    add questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,48 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-});
- 
+    // Question Objects
+    var questions = [
+        {
+            question: "What artist was sentenced to death by the Pope after killing a man?",
+            choices: ["Francisco Goya", "Caravaggio", "Titian", "Raphael"],
+            answer: 1,
+        },
+
+        {
+            question: "Who painted The Persistence of Memory?",
+            choices: ["Salvador Dalí", "Jean-Michel Basquiat", "Gustav Klimt", "Vija Celmins"],
+            answer: 0,
+        },
+
+        {
+            question: "Sold on November 15th 2017 Leonardo da Vinci's <i>Salvator Mundi</i> became the highest pride paid for a painting at what price(in millions)?",
+            choices: ["275", "194.6", "313", "450.3"],
+            answer: 3,
+        },
+
+        {
+            question: "How long did it take Leonardo da Vinci to paint the Mona Lisa's lips?",
+            choices: ["12 days", "12 weeks", "12 years", "12 hours"],
+            answer: 2,
+        },
+
+        {
+            question: "What is the term for visible traces of an earlier painting beneath newer artwork on canvas?",
+            choices: ["Sgraffito", "grisaille", "Pentimento", "Sinopia"],
+            answer: 2,
+        },
+
+        {
+            question: "What art movement literally means the style of 'the wild beasts'?",
+            choices: ["Fauvism", "Dadaism", "primitivism", "modernism"],
+            answer: 0,
+        }];
+    
+
+
+
+        
+
+
+
+})
